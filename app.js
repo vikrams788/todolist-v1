c0a61d0cc476d1e39fa8aebd52da13cce4763bf5
Vikram singh <vikrams788058@gmail.com>
2022-09-14 19:21:59 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,45 @@
+const express=require("express");
+const bodyParser=require("body-parser");
+const date = require(__dirname+"/date.js")
+
+const app=express();
+
+var items = ["Buy Food", "Cook Food", "Eat Food"];
+var workItems = [];
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(express.static("public"));
+
+app.get("/", function(req, res){
+    
+    let day = date();
+    res.render("list", {listTitle: day, newListItems: items});
+});
+
+app.post("/", function(req, res){
+    
+    let item = req.body.newItem;
+
+    if (req.body.list === "Work"){
+       workItems.push(item);
+       res.redirect("/Work")
+    } else {
+        items.push(item);
+        res.redirect("/");
+    }
+});
+
+app.get("/work", function(req, res){
+     res.render("list", {listTitle: "Work List", newListItems: workItems});
+});
+
+app.get("/about", function(req, res){
+    res.render("about");
+});
+
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Server has started");
+});
